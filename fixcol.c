b9a48fa1c63df9cd9eb7a21550f65810b8d5e258
Ryan Itzcovitz <ryanitzcovitz@gmail.com>
2022-05-03 12:35:52 -0300

    Segunda Entrega TP1 (mejorando validación de parámetros, eliminando wrappers innecesarios y lectura de saltos de línea)

--- fixcol.c
+++ fixcol.c
@@ -1,52 +1,61 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include <string.h>
+#include <ctype.h>
+#include <stdbool.h>
 
-FILE* abrir_archivo(char* nombre_archivo, char* modo_archivo) {
-	return fopen(nombre_archivo, modo_archivo);
-}
-
-void cerrar_archivo(FILE* archivo) {
-	fclose(archivo);
+int es_numero(char *n) {
+  size_t i = strlen(n)-1;
+  while (i && isdigit(n[i])) i--;
+  return i == 0 ? 1 : 0;
 }
 
 void dividir_archivo(int cantidad_columnas, FILE* archivo_lectura) {
 	int caracter_leido;
+	bool salto_de_linea = false;
 	int columnas = 0;
 	while ((caracter_leido = fgetc(archivo_lectura)) != EOF) {
-		if (!(columnas == 0 && caracter_leido == '\n')) {
-			fputc(caracter_leido, stdout);
-			columnas++;
+		if (columnas == 0 && caracter_leido == '\n') {
+			if (!salto_de_linea) fputc(caracter_leido, stdout);
+			else salto_de_linea = false;
 		}
-		if (columnas == cantidad_columnas && caracter_leido != '\n') fputc('\n', stdout);
+		else {
+			columnas++;
+			fputc(caracter_leido, stdout);
+		}
+		if (columnas == cantidad_columnas && caracter_leido != '\n') {
+			fputc('\n', stdout);
+			salto_de_linea = true;
+		}
 		if (columnas == cantidad_columnas || caracter_leido == '\n') columnas = 0;
 	}
 }
 
-int validar_cantidad_columnas(int argc, char* argv []) {
-	int cantidad_columnas = 0;
-
-	if (argc < 2 || argc > 3 || (cantidad_columnas = atoi(argv[1])) == 0) fprintf(stderr, "Error: Cantidad erronea de parametros\n");
-	
-	return cantidad_columnas;
+int validar_cantidad_parametros(int argc, char* argv []) {
+	if (argc < 2 || argc > 3 || es_numero(argv[1]) == 0 || atoi(argv[1]) < 1) {
+		fprintf(stderr, "Error: Cantidad erronea de parametros\n");
+		return 0;
+	}
+	return atoi(argv[1]);
 }
 
 FILE* validar_archivo_lectura(int argc, char* argv[]) {
 	FILE* archivo_lectura;
 	
 	if (argc == 2) archivo_lectura = stdin;
-	else if ((archivo_lectura = abrir_archivo(argv[2], "r")) == NULL) fprintf(stderr, "Error: archivo fuente inaccesible\n");
+	else if ((archivo_lectura = fopen(argv[2], "r")) == NULL) fprintf(stderr, "Error: archivo fuente inaccesible\n");
 	
 	return archivo_lectura;
 }
 
 int main(int argc, char* argv []) {
-	int cant_columnas = validar_cantidad_columnas(argc, argv);
+	int cant_columnas = validar_cantidad_parametros(argc, argv);
 	if (cant_columnas == 0) return -1;
 	
 	FILE* archivo_lectura = validar_archivo_lectura(argc, argv);
 	if (archivo_lectura == NULL) return -1;
 	
 	dividir_archivo(cant_columnas, archivo_lectura);
-	cerrar_archivo(archivo_lectura);
+	fclose(archivo_lectura);
 	return 0;
 }
