472da0d15dbd511da4197ba20fae10311917ebd2
Ryan Itzcovitz <ryanitzcovitz@gmail.com>
2022-05-31 15:54:19 -0300

    Añadidos tipos de datos, destacando reversionamiento de vector

---
+++ hash.c
@@ -0,0 +1,311 @@
+#include "hash.h"
+#include "lista.h"
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+#define TAM_INICIAL 101
+#define ALFA_MINIMO 0.25f
+#define ALFA_MAXIMO 2
+#define FACTOR_CREC 2
+#define FACTOR_DEC 0.5f
+#define BASE 256
+
+/* Estructura para almacenar clave, dato */
+typedef struct hash_campo {
+	char* clave;
+	void* dato;
+} hash_campo_t;
+
+/* Estructura del hash: arreglo de punteros a lista. */
+struct hash {
+	lista_t** tabla;
+	size_t cantidad;
+	size_t largo;
+	hash_destruir_dato_t destruir_dato;
+};
+
+/* Estructura del iterador. */
+struct hash_iter {
+	const hash_t* hash;
+	size_t indice;
+	lista_iter_t* iter_actual;
+};
+
+/* *****************************************************************
+ *             	FUNCIÓN DE HASHING
+ * *****************************************************************/
+ 
+unsigned long hashing(const char *s, unsigned long m) {
+	unsigned long h;
+	unsigned const char *us;
+	us = (unsigned const char *) s;
+
+	h = 0;
+	while (*us != '\0') {
+		h = (h * BASE + *us) % m;
+		us++;
+	} 
+
+	return h;
+}
+
+/* *****************************************************************
+ *             FUNCIONES AUXILIARES PARA CAMPO DE HASH
+ * *****************************************************************/
+ 
+char* strdup(const char* org) {
+	char* cpy = malloc(sizeof(char) * (strlen(org) + 1));
+	if (!cpy) return NULL;
+	strcpy(cpy, org);
+	return cpy;
+}
+
+hash_campo_t* hash_campo_crear(const char* clave, void* dato) {
+	hash_campo_t* campo = malloc(sizeof(hash_campo_t));
+	if (campo == NULL) return NULL;
+
+	campo->clave = strdup(clave);
+	campo->dato = dato;
+
+	return campo;
+}
+
+void* hash_campo_destruir(hash_campo_t* campo) {
+	free(campo->clave);
+	void* dato = campo->dato;
+	free(campo);
+	return dato;
+}
+
+void destruir_campo_hash(void* campo) {
+	hash_campo_destruir(campo);
+}
+
+/* *****************************************************************
+ *                 FUNCIONES AUXILIARES PARA HASH
+ * *****************************************************************/
+
+lista_t** tabla_crear(size_t n) {
+	lista_t** tabla = malloc(n*sizeof(lista_t*));
+	if (tabla == NULL) return NULL;
+	
+	for (int i = 0; i < n; i++) {
+		tabla[i] = lista_crear();
+		if (tabla[i] == NULL) {
+			while (i-- >= 0) lista_destruir(tabla[i], NULL);
+			free(tabla);
+			return NULL;
+		}
+	}
+	return tabla;
+}
+
+void tabla_destruir(lista_t** tabla, size_t n, hash_destruir_dato_t destruir_dato, void (*destruir_campo)(void*)) {
+	for (int i = 0; i < n; i++) {
+		if (destruir_dato != NULL) {
+			lista_iter_t* iter = lista_iter_crear(tabla[i]);
+			while(!lista_iter_al_final(iter)) {
+				hash_campo_t* campo = lista_iter_borrar(iter);
+				void* dato = hash_campo_destruir(campo);
+				destruir_dato(dato);
+			}
+			lista_iter_destruir(iter);
+		}
+		lista_destruir(tabla[i], destruir_campo);
+	}
+	free(tabla);
+}
+
+bool llenar_tabla_redmiensionada_a_partir_de_hash(lista_t** tabla_nueva, size_t largo_nuevo, hash_t* hash) {
+	for (int i = 0; i < hash->largo; i++) {
+		lista_iter_t* iter = lista_iter_crear(hash->tabla[i]);
+		if (iter == NULL) {
+			tabla_destruir(tabla_nueva, largo_nuevo, NULL, NULL);
+			return false;
+		}
+	  	while (!lista_iter_al_final(iter)) {
+			hash_campo_t* campo = lista_iter_ver_actual(iter);
+			unsigned long indice = hashing(campo->clave, largo_nuevo);
+			if (!lista_insertar_ultimo(tabla_nueva[indice], campo)) {
+				tabla_destruir(tabla_nueva, largo_nuevo, NULL, NULL);
+					lista_iter_destruir(iter);
+				return false;
+			}
+			lista_iter_avanzar(iter);
+	   	}
+		lista_iter_destruir(iter);
+	}
+	return true;
+}
+
+void reemplazar_tablas_en_hash(hash_t* hash, lista_t** tabla_nueva, size_t largo_nuevo) {
+	lista_t** aux = hash->tabla;
+	hash->tabla = tabla_nueva;
+	tabla_destruir(aux, hash->largo, NULL, NULL);
+	hash->largo = largo_nuevo;	
+}
+
+bool hash_redimensionar(hash_t* hash, float factor_crecimiento) {
+	size_t valor_ajuste = (size_t)(factor_crecimiento*(float)hash->largo);
+	if (valor_ajuste >= TAM_INICIAL) {
+		lista_t** tabla_nueva = tabla_crear(valor_ajuste);
+		if (tabla_nueva == NULL) return false;
+
+		if(!llenar_tabla_redmiensionada_a_partir_de_hash(tabla_nueva, valor_ajuste, hash)) return false;
+		reemplazar_tablas_en_hash(hash, tabla_nueva, valor_ajuste);
+	}
+	return true;
+}
+
+bool redimensionamiento(hash_t* hash) {
+	float alfa = (float)(hash->cantidad/hash->largo);
+	if ((alfa >= ALFA_MAXIMO && !hash_redimensionar(hash, FACTOR_CREC)) || (alfa <= ALFA_MINIMO && !hash_redimensionar(hash, FACTOR_DEC))) return false;
+	return true;
+}
+
+lista_iter_t* encontrar_en_hash_segun_clave(const hash_t* hash, const char* clave) {
+	unsigned long indice = hashing(clave, hash->largo);
+	lista_iter_t* iter = lista_iter_crear(hash->tabla[indice]);
+	if (iter == NULL) return NULL;
+
+	hash_campo_t* campo = lista_iter_ver_actual(iter);
+	while (!lista_iter_al_final(iter) && strcmp(campo->clave,clave) != 0) {
+		lista_iter_avanzar(iter);
+		campo = lista_iter_ver_actual(iter);
+	}
+	return iter;
+}
+
+/* *****************************************************************
+ *            FUNCIONES AUXILIARES PARA ITERADOR DE HASH
+ * *****************************************************************/
+
+void buscar_siguiente_lista_valida(hash_iter_t* iter) {
+	while (iter->indice < iter->hash->largo && lista_esta_vacia(iter->hash->tabla[iter->indice])) iter->indice++;
+	if (iter->indice < iter->hash->largo) iter->iter_actual = lista_iter_crear(iter->hash->tabla[iter->indice]);
+}
+
+/* *****************************************************************
+ *                     PRIMITIVAS DEL HASH
+ * *****************************************************************/
+
+hash_t* hash_crear(hash_destruir_dato_t destruir_dato) {
+	hash_t* hash = malloc(sizeof(hash_t));
+	if (hash == NULL) return NULL;
+
+	hash->largo = TAM_INICIAL;
+	hash->tabla = tabla_crear(hash->largo);
+	if (hash->tabla == NULL) {
+		free(hash);
+		return NULL;
+	}
+	hash->cantidad = 0;
+	hash->destruir_dato = destruir_dato;
+
+	return hash;
+}
+
+bool hash_guardar(hash_t* hash, const char* clave, void* dato) {
+	hash_campo_t* campo = hash_campo_crear(clave, dato);
+	if (campo == NULL) return false;
+	
+	if (!redimensionamiento(hash)) return false;
+
+	if (hash_pertenece(hash, clave)) {
+		void* dato_borrado = hash_borrar(hash, clave);
+		if (hash->destruir_dato != NULL) hash->destruir_dato(dato_borrado);
+	}
+
+	hash->cantidad++;
+	unsigned long indice = hashing(campo->clave, hash->largo);
+	return lista_insertar_ultimo(hash->tabla[indice], campo);
+}
+
+void* hash_borrar(hash_t* hash, const char* clave) {
+	if (!redimensionamiento(hash)) return NULL;
+
+	lista_iter_t* iter = encontrar_en_hash_segun_clave(hash, clave);
+	if (iter == NULL) return NULL;
+	if (lista_iter_al_final(iter)) {
+		lista_iter_destruir(iter);
+		return NULL;
+	}
+
+	hash->cantidad--;
+	hash_campo_t* campo = lista_iter_borrar(iter);
+	lista_iter_destruir(iter);
+	void* dato = hash_campo_destruir(campo);
+	return dato;
+}
+
+void* hash_obtener(const hash_t* hash, const char* clave) {
+	lista_iter_t* iter = encontrar_en_hash_segun_clave(hash, clave);
+	if (iter == NULL) return NULL;
+	if (lista_iter_al_final(iter)) {
+		lista_iter_destruir(iter);
+		return NULL;
+	}
+
+	hash_campo_t* campo = lista_iter_ver_actual(iter);
+	lista_iter_destruir(iter);
+	void* dato = campo->dato;
+	return dato;
+}
+
+bool hash_pertenece(const hash_t* hash, const char* clave) {
+	lista_iter_t* iter = encontrar_en_hash_segun_clave(hash, clave);
+	if (iter == NULL) return false;
+	bool pertenece = !lista_iter_al_final(iter);
+	lista_iter_destruir(iter);
+	return pertenece;
+}
+
+size_t hash_cantidad(const hash_t* hash) {
+	return hash->cantidad;
+}
+
+void hash_destruir(hash_t* hash) {
+	tabla_destruir(hash->tabla, hash->largo, hash->destruir_dato, destruir_campo_hash);
+	free(hash);
+}
+
+/* *****************************************************************
+ *                 PRIMITIVAS DEL ITERADOR DEL HASH
+ * *****************************************************************/
+
+hash_iter_t* hash_iter_crear(const hash_t* hash) {
+	hash_iter_t* iter = malloc(sizeof(hash_iter_t));
+	if (iter == NULL) return NULL;
+
+	iter->hash = hash; 
+	iter->indice = 0;
+	iter->iter_actual = NULL;
+	buscar_siguiente_lista_valida(iter);
+
+	return iter;
+}
+
+bool hash_iter_avanzar(hash_iter_t* iter) {
+	if (iter->iter_actual == NULL) return false;
+	if (lista_iter_avanzar(iter->iter_actual) && !lista_iter_al_final(iter->iter_actual)) return true;
+
+	lista_iter_destruir(iter->iter_actual);
+	iter->iter_actual = NULL;
+	iter->indice++;
+	buscar_siguiente_lista_valida(iter);
+	return iter->indice < iter->hash->largo;
+}
+
+const char* hash_iter_ver_actual(const hash_iter_t* iter) {
+	return hash_iter_al_final(iter) ? NULL : ((hash_campo_t *)lista_iter_ver_actual(iter->iter_actual))->clave;
+}
+
+bool hash_iter_al_final(const hash_iter_t* iter) {
+	return iter->indice == iter->hash->largo;
+}
+
+void hash_iter_destruir(hash_iter_t* iter) {
+	if (iter->iter_actual != NULL) lista_iter_destruir(iter->iter_actual);
+	free(iter);
+}
---
+++ heap.c
@@ -0,0 +1,153 @@
+#include "heap.h"
+#include <stdbool.h>
+#include <stddef.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+
+#define TAM_INICIAL 10
+#define FACTOR_CREC 2
+#define FACTOR_DEC 0.5f
+#define MIN_PROP 4
+#define RAIZ 0
+
+struct heap {
+	void** datos;
+	size_t tam;
+	size_t cant;
+	cmp_func_t cmp;
+};
+
+/* *****************************************************************
+ *                 	FUNCIONES AUXILIARES
+ * *****************************************************************/
+
+bool heap_redimensionar(heap_t* heap, float factor_crecimiento) {
+	size_t valor_ajuste = (size_t)(factor_crecimiento*(float)heap->tam);
+
+	if (valor_ajuste >= TAM_INICIAL) {
+		void** datos = realloc(heap->datos,valor_ajuste*sizeof(void*));
+		if (datos == NULL) return false;
+
+		heap->datos = datos;
+		heap->tam = valor_ajuste;
+	}
+	return true;
+}
+
+void swap_en_arreglo(void** datos, size_t index_1, size_t index_2) {
+	void* aux = datos[index_1];
+	datos[index_1] = datos[index_2];
+	datos[index_2] = aux;
+}
+
+void heap_upheap(void** datos, size_t cant, size_t index_elem, cmp_func_t cmp) {
+	if (index_elem == RAIZ || index_elem >= cant) return;
+
+	size_t index_padre = (index_elem-1)/2;
+	if (cmp(datos[index_padre], datos[index_elem]) < 0) {
+		swap_en_arreglo(datos, index_padre, index_elem);
+		heap_upheap(datos, cant, index_padre, cmp);
+	}
+}
+
+void heap_downheap(void** datos, size_t cant, size_t index_elem, cmp_func_t cmp) {
+	if (cant < 2*index_elem + 2) return;
+
+	size_t index_hijo_izq = 2*index_elem + 1;
+	size_t index_hijo_der = 2*index_elem + 2;
+	size_t index_hijo = cant < index_hijo_der + 1 || cmp(datos[index_hijo_izq], datos[index_hijo_der]) > 0 ? index_hijo_izq : index_hijo_der;
+	if (cmp(datos[index_hijo], datos[index_elem]) > 0) {
+		swap_en_arreglo(datos, index_hijo, index_elem);
+		heap_downheap(datos, cant, index_hijo, cmp);
+	}
+}
+
+void heapify(void** arreglo, size_t n, cmp_func_t cmp) {
+	for (size_t i = n; i > RAIZ; i--) heap_downheap(arreglo, n, i-1, cmp);
+}
+
+void swap_de_raiz_y_downheap(void** arreglo, size_t cant, cmp_func_t cmp) {
+	swap_en_arreglo(arreglo, RAIZ, cant-1);
+	heap_downheap(arreglo, cant-1, RAIZ, cmp);
+}
+
+/* *****************************************************************
+ *                     PRIMITIVAS DEL HEAP
+ * *****************************************************************/
+
+void heap_sort(void* elementos[], size_t cant, cmp_func_t cmp) {
+	heapify(elementos, cant, cmp);
+	for (size_t i = 0; i < cant; i++) swap_de_raiz_y_downheap(elementos, cant-i, cmp);
+}
+
+heap_t* heap_crear(cmp_func_t cmp) {
+	heap_t* heap = malloc(sizeof(heap_t));
+	if (heap == NULL) return NULL;
+	
+	heap->tam = TAM_INICIAL;
+	heap->datos = malloc(heap->tam*sizeof(void*));
+	if (heap->datos == NULL) {
+		free(heap);
+		return NULL;
+	}
+	heap->cant = 0;
+	heap->cmp = cmp;
+	
+	return heap;
+}
+
+heap_t* heap_crear_arr(void* arreglo[], size_t n, cmp_func_t cmp) {
+	heap_t* heap = heap_crear(cmp);
+	if (heap == NULL) return NULL;
+	
+	while (heap->tam < n && heap_redimensionar(heap, FACTOR_CREC));
+	if (heap->tam < n) {
+		heap_destruir(heap, NULL);
+		return NULL;
+	}
+	for (size_t i = 0; i < n; i++) heap->datos[i] = arreglo[i];
+	heap->cant = n;
+
+	heapify(heap->datos, heap->cant, heap->cmp);
+	return heap;
+}
+
+void heap_destruir(heap_t* heap, void (*destruir_elemento)(void* e)) {
+	if (destruir_elemento != NULL) for (size_t i = RAIZ; i < heap->cant; i++) destruir_elemento(heap->datos[i]);
+	free(heap->datos);
+	free(heap);
+}
+
+size_t heap_cantidad(const heap_t* heap) {
+	return heap->cant;
+}
+
+bool heap_esta_vacio(const heap_t* heap) {
+	return heap->cant == 0;
+}
+
+bool heap_encolar(heap_t *heap, void* elem) {
+	if (heap->tam == heap->cant && !heap_redimensionar(heap, FACTOR_CREC)) return false;
+
+	heap->datos[heap->cant] = elem;
+	heap->cant++;
+	heap_upheap(heap->datos, heap->cant, heap->cant-1, heap->cmp);
+	return true;
+}
+
+void* heap_ver_max(const heap_t* heap) {
+	return !heap_esta_vacio(heap) ? heap->datos[RAIZ] : NULL;
+}
+
+void* heap_desencolar(heap_t* heap) {
+	if (heap_esta_vacio(heap)) return NULL;
+
+	if (heap->cant*MIN_PROP <= heap->tam/* && heap->tam > TAM_INICIAL*/) heap_redimensionar(heap, FACTOR_DEC);
+
+	swap_de_raiz_y_downheap(heap->datos, heap->cant, heap->cmp);
+
+	heap->cant--;
+	void* desencolado = heap->datos[heap->cant];
+	return desencolado;
+}
